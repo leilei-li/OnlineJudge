291b78555264bc9ac08acb52181a830f2c8a004f
leilei-li <lileilei258@qq.com>
2017-03-27 09:42:28 +0800

    反转字符串

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner in=new Scanner(System.in);
+        while (in.hasNext()){
+            String str=in.nextLine();
+            for (int i = str.length()-1; i >=0; i--) {
+                System.out.print(str.charAt(i));
+            }
+            System.out.println();
+        }
     }
 }
