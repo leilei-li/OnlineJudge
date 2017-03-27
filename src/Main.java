3fcd1d2bf6397ab4d864942427e116151ef81b2b
leilei-li <lileilei258@qq.com>
2017-03-27 10:15:33 +0800

    sth wrong!

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        int b = in.nextInt();
-        int dif=a^b;
-        int count = 0;
-        while(dif != 0){
-            count++;
-            dif &= (dif-1);
-            System.out.println(dif);
-            System.out.println(dif-1);
+        String str=in.nextLine();
+        int n=in.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(str.charAt(i)==str.charAt(j)){
+                    System.out.println(str.charAt(i));
+                    break;
+                }
+            }
         }
-        System.out.println(count);
     }
 }
