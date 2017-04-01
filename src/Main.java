edfc7ed0db9e7728b8ff644feb971ea444353669
leilei-li <lileilei258@qq.com>
2017-04-01 14:39:51 +0800

    小程序，可以把GBK转为UTF8的程序，用来看电子书的

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,37 @@
-import java.util.Scanner;
+import sun.nio.cs.ext.GBK;
+
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String str=in.nextLine();
-        int n=in.nextInt();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if(str.charAt(i)==str.charAt(j)){
-                    System.out.println(str.charAt(i));
-                    break;
-                }
+//        Solution s=new Solution();
+//        Scanner in = new Scanner(System.in);
+//        while (in.hasNext()) {
+//            String str=in.nextLine();
+//            str=str.substring(2);
+//            System.out.println(Integer.parseInt(str,16));
+//        }
+        File file = new File("/Users/lileilei/Downloads/超品相师.txt");
+        try {
+            InputStreamReader read = new InputStreamReader(new FileInputStream(file), "GBK");
+            BufferedReader bufferedReader = new BufferedReader(read);
+            String str = bufferedReader.readLine();
+            File outFile = new File("/Users/lileilei/Downloads/1.txt");
+            FileOutputStream out = new FileOutputStream(outFile, true); //如果追加方式用true
+            while (str != null) {
+                str = bufferedReader.readLine();
+                String nameGBK = str;
+                byte[] temp = nameGBK.getBytes("GBK");
+                byte[] newtemp = new String(temp, "GBK").getBytes("utf8");
+                String strUtf8 = new String(newtemp, "utf8");
+                System.out.println(strUtf8);
+                out.write(newtemp);
             }
+        } catch (Exception e) {
+            System.out.println("read fail!");
         }
+
     }
 }
---
+++ src/Solution.java
@@ -0,0 +1,6 @@
+/**
+ * Created by lileilei on 2017/3/31.
+ */
+public class Solution {
+
+}
