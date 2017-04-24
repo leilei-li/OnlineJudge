0251e2a968c37e78daa5bb3bec473a275c651d19
leilei-li <lileilei258@qq.com>
2017-04-24 16:59:37 +0800

    1

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,59 @@
-import sun.nio.cs.ext.GBK;
-
-import java.io.*;
 import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-//        Solution s=new Solution();
-//        Scanner in = new Scanner(System.in);
-//        while (in.hasNext()) {
-//            String str=in.nextLine();
-//            str=str.substring(2);
-//            System.out.println(Integer.parseInt(str,16));
-//        }
-        File file = new File("/Users/lileilei/Downloads/超品相师.txt");
-        try {
-            InputStreamReader read = new InputStreamReader(new FileInputStream(file), "GBK");
-            BufferedReader bufferedReader = new BufferedReader(read);
-            String str = bufferedReader.readLine();
-            File outFile = new File("/Users/lileilei/Downloads/1.txt");
-            FileOutputStream out = new FileOutputStream(outFile, true); //如果追加方式用true
-            while (str != null) {
-                str = bufferedReader.readLine();
-                String nameGBK = str;
-                byte[] temp = nameGBK.getBytes("GBK");
-                byte[] newtemp = new String(temp, "GBK").getBytes("utf8");
-                String strUtf8 = new String(newtemp, "utf8");
-                System.out.println(strUtf8);
-                out.write(newtemp);
-            }
-        } catch (Exception e) {
-            System.out.println("read fail!");
-        }
+//        Solution s = new Solution();
+        Scanner in = new Scanner(System.in);
 
+        while (in.hasNext()) {
+            String str = in.nextLine();
+            char[] pw = str.toCharArray();
+            int a1, a2, a3, a4;
+            a1 = a2 = a3 = a4 = 0;
+            if (str.length() < 8) {
+                System.out.println("NG");
+                continue;
+            }
+            if (check(str) == false) {
+                System.out.println("NG");
+                continue;
+            }
+            for (int i = 0; i < pw.length; i++) {
+                if (pw[i] >= '0' && pw[i] <= '9') {
+                    a1 = 1;
+                    continue;
+                }
+                if (pw[i] >= 'a' && pw[i] <= 'z') {
+                    a2 = 1;
+                    continue;
+                }
+                if (pw[i] >= 'A' && pw[i] <= 'Z') {
+                    a3 = 1;
+                    continue;
+                } else {
+                    a4 = 1;
+                    continue;
+                }
+            }
+            int total = a1 + a2 + a3 + a4;
+            if (total >= 3) {
+                System.out.println("OK");
+            } else {
+                System.out.println("NG");
+            }
+
+
+        }
+    }
+
+    public static boolean check(String str) {
+        for (int i = 0; i < str.length() - 2; i++) {
+            String str1 = str.substring(i, i + 3);
+            if (str.substring(i + 1).contains(str1)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
