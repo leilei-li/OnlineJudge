0bc5d8cc32e5e57d26b75aa252b280d5072f420f
leilei-li <lileilei258@qq.com>
2017-04-28 10:42:26 +0800

    leetcode 后缀表示法和二叉树最小深度

--- src/Main.java
+++ src/Main.java
@@ -3,57 +3,11 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-//        Solution s = new Solution();
-        Scanner in = new Scanner(System.in);
-
-        while (in.hasNext()) {
-            String str = in.nextLine();
-            char[] pw = str.toCharArray();
-            int a1, a2, a3, a4;
-            a1 = a2 = a3 = a4 = 0;
-            if (str.length() < 8) {
-                System.out.println("NG");
-                continue;
-            }
-            if (check(str) == false) {
-                System.out.println("NG");
-                continue;
-            }
-            for (int i = 0; i < pw.length; i++) {
-                if (pw[i] >= '0' && pw[i] <= '9') {
-                    a1 = 1;
-                    continue;
-                }
-                if (pw[i] >= 'a' && pw[i] <= 'z') {
-                    a2 = 1;
-                    continue;
-                }
-                if (pw[i] >= 'A' && pw[i] <= 'Z') {
-                    a3 = 1;
-                    continue;
-                } else {
-                    a4 = 1;
-                    continue;
-                }
-            }
-            int total = a1 + a2 + a3 + a4;
-            if (total >= 3) {
-                System.out.println("OK");
-            } else {
-                System.out.println("NG");
-            }
-
-
-        }
+        String[] tokens = new String[]{"2", "1", "+", "3", "*"};
+        Solution s = new Solution();
+        int result=s.evalRPN(tokens);
+        System.out.println(result);
     }
 
-    public static boolean check(String str) {
-        for (int i = 0; i < str.length() - 2; i++) {
-            String str1 = str.substring(i, i + 3);
-            if (str.substring(i + 1).contains(str1)) {
-                return false;
-            }
-        }
-        return true;
-    }
+
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,56 @@
+import java.util.Stack;
+
 /**
  * Created by lileilei on 2017/3/31.
  */
 public class Solution {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int run(TreeNode root) {
+        if (root == null)
+            return 0;
+        if (root.left == null && root.right == null)
+            return 1;
+        if (root.left == null)
+            return run(root.right) + 1;
+        if (root.right == null)
+            return run(root.left) + 1;
+        return Math.min(run(root.left), run(root.right)) + 1;
+    }
+
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<Integer>();
+        int a, b;
+        for (int i = 0; i < tokens.length; i++) {
+            if (tokens[i].equals("+")) {
+                b = stack.pop();//注意栈是先进后出
+                a = stack.pop();
+                stack.push(a + b);
+            } else if (tokens[i].equals("-")) {
+                b = stack.pop();//注意栈是先进后出
+                a = stack.pop();
+                stack.push(a - b);
+            } else if (tokens[i].equals("*")) {
+                b = stack.pop();//注意栈是先进后出
+                a = stack.pop();
+                stack.push(a * b);
+            } else if (tokens[i].equals("/")) {
+                b = stack.pop();//注意栈是先进后出
+                a = stack.pop();
+                stack.push(a / b);
+            } else {
+                int num = Integer.parseInt(tokens[i]);
+                stack.push(num);
+            }
+        }
+        return stack.pop();
+    }
 
 }
