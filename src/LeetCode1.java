a06643e7f00414997f1c88571677482825ea86c6
leilei-li <lileilei258@qq.com>
2017-05-07 22:03:18 +0800

    best time to buy and sell stock i:买卖股票的最好时机 1

--- src/LeetCode1.java
+++ src/LeetCode1.java
@@ -651,4 +651,15 @@ public class LeetCode1 {
         result.set(0, Math.max(result.get(0), node.val + left + right));
         return Math.max(left, right) + node.val;
     }
+
+    public int maxProfit(int[] prices) {
+        if (prices.length == 0) return 0;
+        int profit = 0;
+        int min = prices[0];
+        for (int i = 0; i < prices.length; i++) {
+            min = Math.min(min, prices[i]);
+            profit = Math.max(profit, prices[i] - min);
+        }
+        return profit;
+    }
 }
