9a1d232d7b1cc87bdf211979f4bf7c2851c7a5e1
leilei-li <lileilei258@qq.com>
2017-05-10 10:15:24 +0800

    distinct subsequences：不同的子序列数量

--- src/LeetCode1.java
+++ src/LeetCode1.java
@@ -767,4 +767,24 @@ public class LeetCode1 {
         }
     }
 
+    public int numDistinct(String S, String T) {
+        if (S == null || T == null) return 0;
+        if (T.length() > S.length()) return 0;
+        int[][] dp = new int[S.length() + 1][T.length() + 1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= S.length(); i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i <= S.length(); i++) {
+            for (int j = 1; j <= T.length(); j++) {
+                if (S.charAt(i - 1) != T.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j];
+                } else if (S.charAt(i - 1) == T.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j] + dp[i][j];
+                }
+            }
+        }
+        return dp[S.length()][T.length()];
+    }
+
 }
