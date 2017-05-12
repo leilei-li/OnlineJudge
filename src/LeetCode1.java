bc4f90609ed392f5e28230df6ece8ad7c1b19a95
leilei-li <lileilei258@qq.com>
2017-05-12 15:53:52 +0800

    maximum depth of binary tree: 二叉树最大深度

--- src/LeetCode1.java
+++ src/LeetCode1.java
@@ -922,6 +922,14 @@ public class LeetCode1 {
         return root;
     }
 
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left == null && root.right == null) return 1;
+        if (root.left == null) return maxDepth(root.right) + 1;
+        if (root.right == null) return maxDepth(root.left) + 1;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
     
 
 }
