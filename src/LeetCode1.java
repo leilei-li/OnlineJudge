b131984fd3869acfdb11a2e3e577f615c17b2e8a
leilei-li <lileilei258@qq.com>
2017-05-12 16:28:14 +0800

    binary tree level order traversal: 二叉树层序遍历

--- src/LeetCode1.java
+++ src/LeetCode1.java
@@ -967,4 +967,23 @@ public class LeetCode1 {
         return res;
     }
 
+    public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (queue.isEmpty() == false) {
+            ArrayList<Integer> list = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode curNode = queue.poll();
+                list.add(curNode.val);
+                if (curNode.left != null) queue.offer(curNode.left);
+                if (curNode.right != null) queue.offer(curNode.right);
+            }
+            result.add(list);
+        }
+        return result;
+    }
+
 }
