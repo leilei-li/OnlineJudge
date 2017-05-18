76663a80d38346bd14ae83230a236e5018baacec
leilei-li <lileilei258@qq.com>
2017-05-18 09:13:57 +0800

    remove duplicates from sorted list i：删除链表中的重复节点 1

--- src/LeetCode1.java
+++ src/LeetCode1.java
@@ -1339,4 +1339,16 @@ public class LeetCode1 {
         return result;
     }
 
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) return null;
+        ListNode curNode = head;
+        while (curNode != null) {
+            while (curNode.next != null && curNode.val == curNode.next.val) {
+                curNode.next = curNode.next.next;
+            }
+            curNode = curNode.next;
+        }
+        return head;
+    }
+
 }
