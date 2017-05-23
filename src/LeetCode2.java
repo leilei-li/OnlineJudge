2a5383183f2a0d5c1a1f650c86b0fd43040d5ed0
leilei-li <lileilei258@qq.com>
2017-05-23 16:59:08 +0800

    set matrix zeroes：矩阵元素置零

--- src/LeetCode2.java
+++ src/LeetCode2.java
@@ -26,5 +26,49 @@ public class LeetCode2 {
         }
     }
 
+    public void setZeroes(int[][] matrix) {
+        boolean hasZeroInRow = false;
+        boolean hasZeroInCol = false;
+        int row = matrix.length;
+        int col = matrix[0].length;
+        for (int i = 0; i < row; i++) {
+            if (matrix[i][0] == 0) {
+                hasZeroInCol = true;
+                break;
+            }
+        }
+        for (int i = 0; i < col; i++) {
+            if (matrix[0][i] == 0) {
+                hasZeroInRow = true;
+                break;
+            }
+        }
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (hasZeroInCol) {
+            for (int i = 0; i < row; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        if (hasZeroInRow) {
+            for (int i = 0; i < col; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+    }
+
 
 }
