f24d9678ca968f3342c6c40d66ad450db45aeb40
leilei-li <lileilei258@qq.com>
2017-06-26 10:02:49 +0800

    first missing positive:第一个缺失的正整数

--- src/LeetCode2.java
+++ src/LeetCode2.java
@@ -777,5 +777,25 @@ public class LeetCode2 {
         return result;
     }
 
+    public int firstMissingPositive(int[] A) {
+        int n = A.length;
+        if (n == 1) {
+            if (A[0] <= 0) return 1;
+            if (A[0] == 1) return 2;
+            else return 1;
+        }
+        for (int i = 1; i < n; i++) {
+            while (A[i] > 0 && A[i] <= n && A[A[i] - 1] != A[i]) {
+                int temp = A[A[i] - 1];
+                A[A[i] - 1] = A[i];
+                A[i] = temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (A[i] != i + 1) return i + 1;
+        }
+        return A.length + 1;
+    }
+
 
 }
