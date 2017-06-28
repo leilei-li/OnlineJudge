b303b7dbbef5a77e9d103e3a9e11f44633688493
leilei-li <lileilei258@qq.com>
2017-06-28 10:50:03 +0800

    数独问题

--- src/LeetCode2.java
+++ src/LeetCode2.java
@@ -877,5 +877,91 @@ public class LeetCode2 {
         return stringBuilder.toString();
     }
 
+    public void solveSudoku(char[][] board) {
+        if (board == null || board.length != 9 || board[0].length != 9)
+            return;
+        sudokuHelper(board, 0, 0);
+    }
+
+    private boolean sudokuHelper(char[][] board, int i, int j) {
+        if (j >= 9)
+            return sudokuHelper(board, i + 1, 0);
+        if (i == 9) {
+            return true;
+        }
+        if (board[i][j] == '.') {
+            for (int k = 1; k <= 9; k++) {
+                board[i][j] = (char) (k + '0');
+                if (isValid(board, i, j)) {
+                    if (sudokuHelper(board, i, j + 1))
+                        return true;
+                }
+                board[i][j] = '.';
+            }
+        } else {
+            return sudokuHelper(board, i, j + 1);
+        }
+        return false;
+    }
+
+    private boolean isValid(char[][] board, int i, int j) {
+        for (int k = 0; k < 9; k++) {
+            if (k != j && board[i][k] == board[i][j])
+                return false;
+        }
+        for (int k = 0; k < 9; k++) {
+            if (k != i && board[k][j] == board[i][j])
+                return false;
+        }
+        for (int row = i / 3 * 3; row < i / 3 * 3 + 3; row++) {
+            for (int col = j / 3 * 3; col < j / 3 * 3 + 3; col++) {
+                if ((row != i || col != j) && board[row][col] == board[i][j])
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isValidSudoku(char[][] board) {
+        if (board == null || board.length != 9 || board[0].length != 9)
+            return false;
+        for (int i = 0; i < 9; i++) {
+            boolean[] map = new boolean[9];
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    if (map[(int) (board[i][j] - '1')]) {
+                        return false;
+                    }
+                    map[(int) (board[i][j] - '1')] = true;
+                }
+            }
+        }
+        for (int j = 0; j < 9; j++) {
+            boolean[] map = new boolean[9];
+            for (int i = 0; i < 9; i++) {
+                if (board[i][j] != '.') {
+                    if (map[(int) (board[i][j] - '1')]) {
+                        return false;
+                    }
+                    map[(int) (board[i][j] - '1')] = true;
+                }
+            }
+        }
+        for (int block = 0; block < 9; block++) {
+            boolean[] map = new boolean[9];
+            for (int i = block / 3 * 3; i < block / 3 * 3 + 3; i++) {
+                for (int j = block % 3 * 3; j < block % 3 * 3 + 3; j++) {
+                    if (board[i][j] != '.') {
+                        if (map[(int) (board[i][j] - '1')]) {
+                            return false;
+                        }
+                        map[(int) (board[i][j] - '1')] = true;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
 
 }
