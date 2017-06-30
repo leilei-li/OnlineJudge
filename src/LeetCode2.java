b0001cd4b2f77091a468b0ca594add8b04c2bae1
leilei-li <lileilei258@qq.com>
2017-06-30 11:20:37 +0800

    substring with concatenation of all words: 子串字典匹配

--- src/LeetCode2.java
+++ src/LeetCode2.java
@@ -986,5 +986,107 @@ public class LeetCode2 {
         return new int[]{-1, -1};
     }
 
+    public int longestValidParentheses(String s) {
+        if (s.length() == 0) return 0;
+        int len = 0, last = -1;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') stack.push(i);
+            else {
+                if (stack.isEmpty()) last = i;
+                else {
+                    stack.pop();
+                    if (stack.isEmpty()) len = Math.max(len, i - last);
+                    else len = Math.max(len, i - stack.peek());
+                }
+            }
+        }
+        return len;
+    }
+
+    public void nextPermutation(int[] num) {
+        if (num == null || num.length == 0)
+            return;
+        int i = num.length - 2;
+        while (i >= 0 && num[i] >= num[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = i + 1;
+            while (j < num.length && num[j] > num[i]) {
+                j++;
+            }
+            j--;
+            int temp = num[i];
+            num[i] = num[j];
+            num[j] = temp;
+        }
+        int l = i + 1;
+        int r = num.length - 1;
+        while (l < r) {
+            int temp = num[l];
+            num[l] = num[r];
+            num[r] = temp;
+            l++;
+            r--;
+        }
+    }
+
+    public ArrayList<Integer> findSubstring(String S, String[] L) {
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        if (S == null || S.length() == 0 || L == null || L.length == 0)
+            return res;
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        for (int i = 0; i < L.length; i++) {
+            if (map.containsKey(L[i])) {
+                map.put(L[i], map.get(L[i]) + 1);
+            } else {
+                map.put(L[i], 1);
+            }
+        }
+        for (int i = 0; i < L[0].length(); i++) {
+            HashMap<String, Integer> curMap = new HashMap<String, Integer>();
+            int count = 0;
+            int left = i;
+            for (int j = i; j <= S.length() - L[0].length(); j += L[0].length()) {
+                String str = S.substring(j, j + L[0].length());
+
+                if (map.containsKey(str)) {
+                    if (curMap.containsKey(str))
+                        curMap.put(str, curMap.get(str) + 1);
+                    else
+                        curMap.put(str, 1);
+                    if (curMap.get(str) <= map.get(str))
+                        count++;
+                    else {
+                        while (curMap.get(str) > map.get(str)) {
+                            String temp = S.substring(left, left + L[0].length());
+                            if (curMap.containsKey(temp)) {
+                                curMap.put(temp, curMap.get(temp) - 1);
+                                if (curMap.get(temp) < map.get(temp))
+                                    count--;
+                            }
+                            left += L[0].length();
+                        }
+                    }
+                    if (count == L.length) {
+                        res.add(left);
+                        String temp = S.substring(left, left + L[0].length());
+                        if (curMap.containsKey(temp))
+                            curMap.put(temp, curMap.get(temp) - 1);
+                        count--;
+                        left += L[0].length();
+                    }
+                } else {
+                    curMap.clear();
+                    count = 0;
+                    left = j + L[0].length();
+                }
+            }
+        }
+        Collections.sort(res);
+        return res;
+    }
+
 
 }
