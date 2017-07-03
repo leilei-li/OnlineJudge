055f13a702c364b766b520e12abfc396ff06bcc0
leilei-li <lileilei258@qq.com>
2017-07-03 09:08:31 +0800

    divide two integer: 模拟除法

--- src/LeetCode2.java
+++ src/LeetCode2.java
@@ -1088,5 +1088,29 @@ public class LeetCode2 {
         return res;
     }
 
+    public int divide(int dividend, int divisor) {
+        int sign = 1;
+        if (dividend < 0) sign = -sign;
+        if (divisor < 0) sign = -sign;
+        long temp = Math.abs((long) dividend);
+        long temp2 = Math.abs((long) divisor);
+        long c = 1;
+        while (temp > temp2) {
+            temp2 = temp2 << 1;
+            c = c << 1;
+        }
+        int res = 0;
+        while (temp >= Math.abs((long) divisor)) {
+            while (temp >= temp2) {
+                temp -= temp2;
+                res += c;
+            }
+            temp2 = temp2 >> 1;
+            c = c >> 1;
+        }
+        if (sign > 0) return res;
+        else return -res;
+    }
+
 
 }
