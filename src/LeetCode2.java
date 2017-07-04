ae683aa5cb50c53575a1edbbcd30dea385edf9cc
leilei-li <lileilei258@qq.com>
2017-07-04 15:39:52 +0800

    reverse nodes in k group: 逆转链表前K位

--- src/LeetCode2.java
+++ src/LeetCode2.java
@@ -1138,5 +1138,32 @@ public class LeetCode2 {
         return index;
     }
 
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || k < 2) return head;
+        ListNode first = new ListNode(-1);
+        first.next = head;
+        ListNode pre = first;
+        ListNode cur = head;
+        ListNode temp = null;
+        int len = 0;
+        while (head != null) {
+            head = head.next;
+            len++;
+        }
+        int count = len / k;
+        while (count-- > 0) {
+            int c = k;
+            while (c-- > 1) {
+                temp = cur.next;
+                cur.next = temp.next;
+                temp.next = pre.next;
+                pre.next = temp;
+            }
+            pre = cur;
+            cur = cur.next;
+        }
+        return first.next;
+    }
+
 
 }
