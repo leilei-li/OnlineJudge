33d74992c31e1a9ea8a7fdb1ea0d48c342a65056
leilei-li <lileilei258@qq.com>
2017-07-05 16:30:43 +0800

    palindrome number: 回文数字

--- src/LeetCode2.java
+++ src/LeetCode2.java
@@ -1412,5 +1412,19 @@ public class LeetCode2 {
         return result;
     }
 
+    public boolean isPalindrome(int x) {
+        if (x < 0)
+            return false;
+        int div = 1;
+        while (div <= x / 10)
+            div *= 10;
+        while (x > 0) {
+            if (x / div != x % 10)
+                return false;
+            x = (x % div) / 10;
+            div /= 100;
+        }
+        return true;
+    }
 
 }
