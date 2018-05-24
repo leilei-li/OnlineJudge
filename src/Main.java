ad660eace3d746ae103d9de0182c08195ad7189f
leilei-li <lileilei258@qq.com>
2018-05-24 11:40:29 +0800

    first commit with idea

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,18 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        LeetCode1 leetCode1 = new LeetCode1();
-        LeetCode2 leetCode2 = new LeetCode2();
-        swordToOffer swordToOffer = new swordToOffer();
-        //leetCode1.grayCode(6);
-        //System.out.println(leetCode1.candy(new int[]{88,72,89,73,80}));
-        //System.out.println(leetCode2.minDistance("happy", "yppah2"));
-        //System.out.println(swordToOffer.JumpFloor(24));
+//        LeetCode1 leetCode1 = new LeetCode1();
+//        LeetCode2 leetCode2 = new LeetCode2();
+//        swordToOffer swordToOffer = new swordToOffer();
+//        //leetCode1.grayCode(6);
+//        //System.out.println(leetCode1.candy(new int[]{88,72,89,73,80}));
+//        //System.out.println(leetCode2.minDistance("happy", "yppah2"));
+//        //System.out.println(swordToOffer.JumpFloor(24));
+        Scanner scanner = new Scanner(System.in);
+        Integer a = scanner.nextInt();
+        Integer b = scanner.nextInt();
+        System.out.println(a + b);
+
     }
 
 }
