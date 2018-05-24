5e61b85e907b87a8801e24e7be2c460eef1a86f9
leilei-li <lileilei258@qq.com>
2018-05-24 11:57:23 +0800

    add LeetCode3

---
+++ src/LeetCode3.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.math.*;
+
+public class LeetCode3 {
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int run(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        if (root.left == null) {
+            return run(root.right) + 1;
+        }
+        if (root.right == null) {
+            return run(root.left) + 1;
+        }
+        return Math.min(run(root.left), run(root.right)) + 1;
+    }
+}
