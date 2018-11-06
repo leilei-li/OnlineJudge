1741aed56f3e37266fc95d3d1824b5f03e6deb08
leilei-li <lileilei258@qq.com>
2018-11-06 16:39:38 +0800

    add LeetCode3

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         LeetCode2 l2 = new LeetCode2();
         int[] A = new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
-        l2.trap(A);
+        System.out.println(l2.trap(A));
 
     }
 
